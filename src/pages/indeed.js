456bd8f09c6b8ddd66cfcd98b8fc6d62b8d03d5d
brett <brett.bartylla@outlook.com>
2022-09-22 21:23:08 -0500

    feat(projects): started indeed project page

--- src/pages/indeed.js
+++ src/pages/indeed.js
@@ -4,8 +4,9 @@ import Navbar from "../components/Navbar"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Img from 'gatsby-image'
 
-const NotFoundPage = () => (
+const NotFoundPage = ({data}) => (
   <Layout>
     <Seo title="Indeed" />
     <Navbar />
@@ -22,9 +23,53 @@ const NotFoundPage = () => (
           New hiring feature marketing video for Indeed
         </li>
       </ul>
+      <Img 
+        fluid={data.indeed_1GatsbyImage.childImageSharp.fluid}
+        imgStyle={{ objectFit: 'contain' }}
+        alt="Indeed Project 1"
+      />
+      <ul>
+        <li>
+          Campaign Marketing Video
+        </li>
+        <li>
+          Graphic design, animation, video editing, copywriting
+        </li>
+        <li>
+          New hiring feature marketing video for Indeed
+        </li>
+      </ul>
+      <Img 
+        fluid={data.indeed_2GatsbyImage.childImageSharp.fluid}
+        imgStyle={{ objectFit: 'contain' }}
+        alt="Indeed Project 1"
+      />
+
 
     </div>
   </Layout>
 )
 
 export default NotFoundPage
+
+
+
+// image queries
+export const query = graphql`
+  query {
+    indeed_1GatsbyImage: file(relativePath: {eq: "projects/indeed/indeed_01.webp"}) {
+      childImageSharp {
+        fluid(maxWidth: 2000) {
+        ...GatsbyImageSharpFluid
+        }
+      }
+    },
+    indeed_2GatsbyImage: file(relativePath: {eq: "projects/indeed/indeed_02.webp"}) {
+      childImageSharp {
+        fluid(maxWidth: 2000) {
+        ...GatsbyImageSharpFluid
+        }
+      }
+    },
+  }
+`
