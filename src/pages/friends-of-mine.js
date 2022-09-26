177a961a6f1908df40c9ec1672786c4f3449b780
brett <brett.bartylla@outlook.com>
2022-09-26 16:34:46 -0500

    feat(projects): finish friends of mine page

--- src/pages/friends-of-mine.js
+++ src/pages/friends-of-mine.js
@@ -6,22 +6,49 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 import Img from 'gatsby-image'
 
-
-
 const NotFoundPage = ({data}) => (
   <Layout>
-    <Seo title="Projects | FRIENDS OF MINE" />
+    <Seo title="Projects | 3M" />
     <Header />
     <Navbar />
     <div className="Projects">
-      <h4>FRIENDS OF MINE</h4>
-      {/* <Img 
-        fluid={data.mbg_1GatsbyImage.childImageSharp.fluid}
-        imgStyle={{ objectFit: 'contain' }}
-        alt="ROGÊ Project 1"
-      /> */}
+      <h4>3M</h4>
+      <div className="desc-container-wide m-b-15">
+        <article>
+          <p>
+            UX and motion design
+          </p>
+        </article>
+        <article>
+          <p>
+            Website for paper product brand <a className="link" href="http://www.friendsofmine.shop">Friends of Mine</a> 
+          </p>
+        </article>
+      </div>
+      <a href="http://www.friendsofmine.shop" target="_blank">
+      <Img 
+          fluid={data.fom_1GatsbyImage.childImageSharp.fluid}
+          imgStyle={{ objectFit: 'contain' }}
+          alt="Friends of Mine"
+          className="m-b-50"
+        />
+      </a>
+
     </div>
   </Layout>
 )
 
 export default NotFoundPage
+
+// image queries
+export const query = graphql`
+  query {
+    fom_1GatsbyImage: file(relativePath: {eq: "projects/friendsofmine/fom_01.webp"}) {
+      childImageSharp {
+        fluid(maxWidth: 2000, quality: 90) {
+        ...GatsbyImageSharpFluid
+        }
+      }
+    },
+  }
+`
