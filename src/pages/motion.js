64dbd318525752c3c4ea53a750ab47a1c59e3936
brett <brett.bartylla@outlook.com>
2022-09-26 16:46:25 -0500

    feat(projects): add motion video

--- src/pages/motion.js
+++ src/pages/motion.js
@@ -38,12 +38,12 @@ const NotFoundPage = ({data}) => (
         <div className="desc-container-wide m-t-25 m-b-15">
           <article>
             <p>
-              So Nice
+              Mango drive
             </p>
           </article>
           <article>
             <p>
-              Sound visualizer created in p5.js for Summer Samba by Astrrud Gilberto
+              Motion exercise
             </p>
           </article>
         </div>
@@ -55,6 +55,27 @@ const NotFoundPage = ({data}) => (
           allowfullscreen
           frameborder="0"
         ></iframe>
+
+        <div className="desc-container-wide m-t-25 m-b-15">
+          <article>
+            <p>
+              So Nice
+            </p>
+          </article>
+          <article>
+            <p>
+              Sound visualizer created in p5.js for Summer Samba by Astrrud Gilberto
+            </p>
+          </article>
+        </div>
+      <iframe
+        src="https://player.cloudinary.com/embed/?public_id=v1664228498%2Fclaire-portfolio%2Fmotion%2Fvideos%2Fvideo_03.mp4&cloud_name=dfqto9ozl&player%5Bfluid%5D=true&player%5Bcontrols%5D=true&player%5Bmuted%5D=true&player%5Bautoplay%5D=true&player%5Bloop%5D=true&source%5BsourceTypes%5D%5B0%5D=mp4"
+        width="640"
+        height="480"
+        allow="autoplay; fullscreen; encrypted-media; picture-in-picture"
+        allowfullscreen
+        frameborder="0"
+      ></iframe>
 
       </div>
     </div>
