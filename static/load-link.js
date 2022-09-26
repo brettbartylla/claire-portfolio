51471feafdb656c322ada277e53b0eed778e79dd
brett <brett.bartylla@outlook.com>
2022-09-26 18:54:22 -0500

    style: add js file for removing underline after page load

---
+++ static/load-link.js
@@ -0,0 +1,7 @@
+
+const loadNavLink = document.getElementsByClassName('loadNavLink');
+const navLinks = document.getElementsByClassName('navLinks');
+
+navLinks.addEventListener('click', function handleClick() {
+  navLink.classList.remove("loadNavLink");
+});
