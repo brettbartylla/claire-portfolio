cdbfc47758cf689b41c43d5ab2501d77f4e65842
brett <brett.bartylla@outlook.com>
2022-09-26 15:19:26 -0500

    feat(projects): add remining gifs to roge

--- src/pages/roge.js
+++ src/pages/roge.js
@@ -6,18 +6,39 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 import Img from 'gatsby-image'
 
+
 const NotFoundPage = ({data}) => (
   <Layout>
-    <Seo title="Projects | ROGÊ" />
+    <Seo title="ROGÊ" />
     <Header />
     <Navbar />
     <div className="Projects">
       <h4>ROGÊ</h4>
-      {/* <Img 
-        fluid={data.mbg_1GatsbyImage.childImageSharp.fluid}
-        imgStyle={{ objectFit: 'contain' }}
-        alt="ROGÊ Project 1"
-      /> */}
+      <div className="desc-container m-b-15">
+        <article>
+          <p>
+            Pra Vida Lyric Video
+          </p>
+        </article>
+        <article>
+          <p>
+            Typography and motion design
+          </p>
+        </article>
+      </div>
+
+      <div className="grid-2x2">
+        <img src="https://res.cloudinary.com/dfqto9ozl/image/upload/f_webp/v1664222355/claire-portfolio/rog%C3%AA/pravida_gif_01.gif" alt="Music video gif" />
+        <img src="https://res.cloudinary.com/dfqto9ozl/image/upload/f_webp/v1664222745/claire-portfolio/rog%C3%AA/pravida_gif_02.gif" alt="Music video gif" />
+        <img src="https://res.cloudinary.com/dfqto9ozl/image/upload/f_webp/v1664222355/claire-portfolio/rog%C3%AA/pravida_gif_04.gif" alt="Music video gif" />
+        <img src="https://res.cloudinary.com/dfqto9ozl/image/upload/f_webp/v1664222352/claire-portfolio/rog%C3%AA/pravida_gif_03.gif" alt="Music video gif" />
+      </div>
+      <img src="https://res.cloudinary.com/dfqto9ozl/image/upload/f_webp/v1664222745/claire-portfolio/rog%C3%AA/pravida_gif_07.gif" alt="Music video gif" className="p-t-15 p-b-10" />
+
+      <div className="grid-2x2">
+        <img src="https://res.cloudinary.com/dfqto9ozl/image/upload/f_webp/v1664222351/claire-portfolio/rog%C3%AA/pravida_gif_05.gif" alt="Music video gif" />
+        <img src="https://res.cloudinary.com/dfqto9ozl/image/upload/f_webp/v1664222746/claire-portfolio/rog%C3%AA/pravida_gif_06.gif" alt="Music video gif" />
+      </div>
     </div>
   </Layout>
 )
