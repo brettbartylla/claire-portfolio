5a836766d780123e5b7c5b69d593ae6a061c6e6e
brett <brett.bartylla@outlook.com>
2022-09-26 22:32:07 -0500

    feat(pages): update home

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,11 +9,22 @@ import { withPrefix } from "gatsby"
 
 const IndexPage = ({data}) => (
   <Layout>
-    <Seo title="Home" />
+    <Seo title="Info" />
     <Header />
     <Navbar />
-    <div className="Grid--landing">
-      <div></div>
+    <div className="Projects info">
+        <p>
+            Claire Loes is a freelance graphic designer and artist currently based 
+            in Minneapolis, Minnesota. In December 2020 they earned their BFA in Graphic 
+            Design from the Minneapolis College of Art and Design.
+        </p>
+        <p>
+            Loes' work embraces an intersection of handmade and digital image-making. They are currently inspired by the music of Lena Platonos and the collaborative work of Nam June Paik and Charlotte Moorman. 
+        </p>
+
+        <p>
+            This fall, Claire is knitting and making videos in their spare time.
+        </p>
     </div>
   </Layout>
 )
